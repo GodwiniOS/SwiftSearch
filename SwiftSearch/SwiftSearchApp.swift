f569ea4b7ab9d51a042bb01cb639ae814cb332d8
Godwin A <iamgodwin10@gmail.com>
2021-12-04 16:50:41 +0400

    Initial Commit

---
+++ SwiftSearch/SwiftSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSearchApp.swift
+//  SwiftSearch
+//
+//  Created by Godwin A on 04/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
